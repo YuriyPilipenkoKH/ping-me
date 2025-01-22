4cf5e59e6c9e4a112b6034f2fcce7bb23e08d9a4
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-22 23:00:34 +0200

    created xpress.Router()

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import express from 'express';
 dotenv.config();
 const PORT = process.env.PORT || 5500;
 
+app.use('/api/auth', authRoutes);
+
 const app = express();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

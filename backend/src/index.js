ada9270828809c1fee02ab16e13a79d8fc665056
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-22 23:12:40 +0200

    update main entry point to src/index.js and reorganize app initialization

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,10 @@ import authRoutes from './routes/auth.route.js';
 dotenv.config();
 const PORT = process.env.PORT || 5500;
 
+const app = express();
 app.use('/api/auth', authRoutes);
 
-const app = express();
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

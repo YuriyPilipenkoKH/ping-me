bdc86b4f42a0b327252679ff8c0b05f901f9292b
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-23 18:26:30 +0200

    getUsersForSidebar() template

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { checkAuth, login, logout, signup, updateProfile } from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
+import { getUsersForSidebar } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
-router.post('/signup', signup );
+router.get('/users', protectRoute , getUsersForSidebar );
 
 
 export default router;

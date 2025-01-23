306e6f298d3542b148fd390ba1a06b8b8b8c737f
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-23 14:15:07 +0200

    completed middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import User from '../models/user.model'
 
 export const protectRoute = async (req,res,next) => {
   try {
@@ -6,6 +7,15 @@ export const protectRoute = async (req,res,next) => {
     if(!token) {
       return res.status(401).json({message: 'Unauthorized — No token provided'})
     }
+    const decoded = jwt.verify(token, process.env.SECRET)
+    const user = User.findById(decoded.userId).select("-password")
+    
+    if(!user) {
+      return res.status(401).json({message: "User not found"})
+    }
+    req.user = user 
+    next()
+
   } catch (error) {
     
   }

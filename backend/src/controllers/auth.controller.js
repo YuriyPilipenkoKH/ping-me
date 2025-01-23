3505761934d3e33a5ae4cae67e652c4f8ba91fcb
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-23 14:56:54 +0200

    completed checkAuth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import User from '../models/user.model.js';
 import generateToken from '../lib/utils.js';
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup = async(req, res) => {
   const{ name, email, password } = req.body;
@@ -101,5 +102,34 @@ export const logout = () => {
 }
 
 export const updateProfile = async(req, res) => {
+  try {
+    const {profilePic} =  req.body
+    const userId = req.user._id
 
+    if(!profilePic) {
+     return res.status(400).json({
+      message: " Profile pic is not provided"
+      })
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+    const updateUser = await User.findByIdAndUpdate(
+      userId, 
+      {profilePic: uploadResponse.secure_url},
+      {new: true}
+    )
+    return res.status(200).json({updateUser})
+    
+  } catch (error) {
+    console.log('error in updateprofile controller');
+  }
+}
+
+export const checkAuth = (req,res) => {
+  try {
+    res.status(200).json(req.user)
+  } catch (error) {
+    console.log("error in checkauth controller ",error.message);
+    res.status(500).json({message: 'server error'})
+  }
 }

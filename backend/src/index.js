c89de1f9b0c879a575a1f9c8ca4751ef9833e77a
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-23 18:20:08 +0200

    creating endpoint for messages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 import connectDB from './lib/db.js';
 import cookieParser from 'cookie-parser'
 
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

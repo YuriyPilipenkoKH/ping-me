581f76b8fe31040e40025a9d2de8a032144a57c1
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-23 19:45:46 +0200

    grab all vars from request

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -36,8 +36,9 @@ export const getMessages =  async (req,res) => {
 
 export const sendMessage =  async (req,res) => {
   try {
-    
-    
+    const {text,image} = req.body
+    const {id: receiverId} = req.params
+
   } catch (error) {
     console.log("error in sendMessage");
     return res.status(500).json({ message: 'Server error' });

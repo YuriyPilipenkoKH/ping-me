c8f03dc274ce36ba0639ef4c8ad550cfc0dbc17f
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-23 18:42:27 +0200

    got my users

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,15 @@
+import User from "../models/user.model.js"
 
 
 export const getUsersForSidebar = async (req,res) => {
-  
+  try {
+    const loggedInUserId = req.user._id //me
+    const filteredUsers = await User
+    .find({_id: {$ne:loggedInUserId}}) // find all exept me
+    .select("-password")
+
+    return res.status(200).json(filteredUsers)
+  } catch (error) {
+    
+  }
 }

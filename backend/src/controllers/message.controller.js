11268b906fef76e6f739d6fc452fcd47c21489a8
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-23 19:55:02 +0200

    completed sendMessage func

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -30,7 +30,7 @@ export const getMessages =  async (req,res) => {
     })
     return res.status(200).json(messages)
   } catch (error) {
-    console.log("error in getMessages");
+    console.log("error in getMessages controller");
     return res.status(500).json({ message: 'Server error' });
   }
 }
@@ -46,10 +46,18 @@ export const sendMessage =  async (req,res) => {
       //upload base64
       const uploadResponse = await cloudinary.uploader.upload(image)
       imageUrl = uploadResponse.secure_url
-    }
+     }
 
+     const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image:imageUrl
+     })
+     await newMessage.save()
+  return res.status(200).json(newMessage)
   } catch (error) {
-    console.log("error in sendMessage");
+    console.log("error in sendMessage controller");
     return res.status(500).json({ message: 'Server error' });
   }
 }

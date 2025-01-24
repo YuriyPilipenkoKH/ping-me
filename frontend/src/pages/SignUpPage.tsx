ae56438f1dca90f6b223b0de2b1a895e2c9cbd08
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-24 22:45:57 +0200

    made responsive signup page

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,13 +1,28 @@
 import { useState } from "react"
 import { SignUpForm } from "../components/forms/SignUpForm"
+import Logo from "../components/nav/Logo"
+import { Link } from "react-router-dom"
 
 
 
 const SignUpPage = () => {
   const [show, setShow] = useState<boolean>(false)
   return (
-    <div>
-    <SignUpForm />
+    <div className="min-h-screen grid lg:grid-cols-2">
+      <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+        <div className="w-full max-w-md space-y-8">
+          <Logo/>
+        <SignUpForm />
+        <div className="text-center">
+            <p className="text-base-content/60">
+              Already have an account?{" "}
+              <Link to="/login" className="link link-primary">
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }

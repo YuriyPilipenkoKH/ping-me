affb4f4431e1f99abdb7fcd2257edab37ab0e89e
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-24 16:31:02 +0200

    configured tailwind & daisy=ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-teal-800 flex items-center justify-center p-10'>
+    <div className='text-teal-900 flex p-10 flex-col'>
       <h2>App</h2>
+      <form >
+        <input type="text" className="input input-secondary "/>
+      </form>
       </div>
   )
 }

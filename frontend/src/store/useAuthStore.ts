36214a91fb0e5624bd36bfad24d0369a146495e2
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-24 19:03:15 +0200

    filled store with vars

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,15 +1,28 @@
 import {create} from 'zustand'
 import { User } from '../types/userTypes';
 
-interface AuthStore {
-  authUser: User | null; // User is the interface from `userTypes.ts`
-  isCheckingAuth: boolean;
+interface AuthStoreTypes {
+  authUser: User | null // User is the interface from `userTypes.ts`
+  isCheckingAuth: boolean
   // setAuthUser: (user: User | null) => void; // Action to update `authUser`
   // setIsCheckingAuth: (isChecking: boolean) => void; // Action to update `isCheckingAuth`
-
+  isSigningUp: boolean
+  isLoggingdIn: boolean
+  isUpdatingProfile: boolean
+  checkAuth: () =>void
 }
 
-export const useAuthStore = create<AuthStore>((set) => ({
+export const useAuthStore = create<AuthStoreTypes>((set) => ({
   authUser: null,
-  isCheckingAuth: true
+  isCheckingAuth: true,
+  isSigningUp: true,
+  isLoggingdIn: true,
+  isUpdatingProfile: true,
+  checkAuth: async() =>{
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
 }))

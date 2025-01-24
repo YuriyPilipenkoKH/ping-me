9115e826771aaaa64ac743546697178037be485f
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-24 16:59:37 +0200

    all imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import SignUpPage from "./pages/SignUpPage"
 import LoginPage from "./pages/LoginPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
 
 const App = () => {
 

44e81fa043d96973def53a1cabdc5be5c26b96b3
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-24 17:51:13 +0200

    types to store

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -1,6 +1,15 @@
 import {create} from 'zustand'
+import { User } from '../types/userTypes';
 
-export const useAuthStore = create((set) => ({
+interface AuthStore {
+  authUser: User | null; // User is the interface from `userTypes.ts`
+  isCheckingAuth: boolean;
+  // setAuthUser: (user: User | null) => void; // Action to update `authUser`
+  // setIsCheckingAuth: (isChecking: boolean) => void; // Action to update `isCheckingAuth`
+
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({
   authUser: null,
   isCheckingAuth: true
 }))
---
+++ frontend/src/types/userTypes.ts
@@ -0,0 +1,10 @@
+
+
+export interface User {
+  name: string
+  email: string
+  password: string
+  image: string
+  role: string
+
+}

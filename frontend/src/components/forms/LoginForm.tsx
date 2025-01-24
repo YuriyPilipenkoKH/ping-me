e95187a2616082d6a59e7a7fedffc15b3351f45a
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-24 23:03:12 +0200

    set up react-hook-form vars

--- frontend/src/components/forms/LoginForm.tsx
+++ frontend/src/components/forms/LoginForm.tsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import React, { useState } from 'react'
+import { loginSchema, LoginSchemaType } from '../../models/loginSchema'
+import { useForm } from 'react-hook-form'
 
 const LoginForm = () => {
+  const [logError, setLogError] = useState<string>('')
+   const {
+      register, 
+      handleSubmit,
+      formState,
+      reset,
+    } = useForm<LoginSchemaType >({
+      defaultValues: {  
+          email: '',
+          password: '' 
+         },
+          mode:'all',
+          resolver: zodResolver(loginSchema), })
+    const {
+      // errors,
+      // isDirty,
+      // isValid ,
+      // isSubmitting,
+      // isLoading 
+    } = formState
   return (
     <div>LoginForm</div>
   )

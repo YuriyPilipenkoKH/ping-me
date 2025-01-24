820006f31cf32ef1f8a9a228188b2912a953b128
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-24 19:23:49 +0200

    installeed cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import connectDB from './lib/db.js';
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 dotenv.config();
 const PORT = process.env.PORT || 5500;
@@ -13,6 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
+app.use(cors({
+  origin:'http://localhost:5173',
+  credentials:true
+}))
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);

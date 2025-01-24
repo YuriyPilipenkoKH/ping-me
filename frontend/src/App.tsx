06bcccaef82ab9610bb77af8baaa5f2650c1dd08
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-24 16:40:44 +0200

    set up BrowserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
 
 const App = () => {
   return (
-    <div className='text-teal-900 flex p-10 flex-col'>
-      <h2>App</h2>
-      <form >
-        <input type="text" className="input input-secondary "/>
-      </form>
+    <div className=' flex p-10 flex-col'>
+
       </div>
   )
 }

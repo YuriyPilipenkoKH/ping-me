e8f1e876e08f04ff20378e16a5fc9f99029c88c5
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-24 21:15:08 +0200

    completed signUpForm

--- frontend/src/components/forms/signUpForm.tsx
+++ frontend/src/components/forms/signUpForm.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { FieldErrors, useForm } from 'react-hook-form'
+import {  useForm } from 'react-hook-form'
 import { signUpSchemaType ,signUpSchema} from '../../models/signUpSchema'
+import { cn } from '../../lib/cn'
 
 const signUpForm = () => {
   const [logError, setLogError] = useState<string>('')
@@ -19,14 +20,66 @@ const signUpForm = () => {
         mode:'all',
         resolver: zodResolver(signUpSchema), })
   const {
-    // errors,
-    // isDirty,
-    // isValid ,
-    // isSubmitting,
-    // isLoading 
-} = formState
+    errors,
+    isDirty,
+    isValid ,
+    isSubmitting,
+    isLoading 
+  } = formState
+  const onSubmit = async (data: signUpSchemaType) => {
+    const formData = new FormData();
+    formData.append('name', data.name);
+    formData.append('email', data.email);
+    formData.append('password', data.password);
+    console.log(data);
+  }
+  const handleInputChange =   (field: keyof signUpSchemaType) => {
+    if(logError) setLogError('')
+    }
   return (
-    <div>signUpForm</div>
+    <form  onSubmit={handleSubmit(onSubmit)}
+    className=''
+    autoComplete="off"
+    noValidate>
+      <label className={cn('formLabel input input-bordered flex items-center gap-2')}>
+        <input 
+          className={cn('grow' )}
+          {...register('name', { onChange: handleInputChange })}
+            placeholder=	{( isSubmitting ) 
+              ? "Processing" 
+              : 'name'}
+          />
+          {errors.name && <div>{errors.name.message}</div>}
+      </label>
+      <label className={cn('formLabel input input-bordered flex items-center gap-2')}>
+        <input 
+          className={cn('grow' )}
+          {...register('email', { onChange: handleInputChange })}
+            placeholder=	{( isSubmitting ) 
+              ? "Processing" 
+              : 'email'}
+          />
+          {errors.email && <div>{errors.email.message}</div>}
+      </label>
+      <label className={cn('formLabel input input-bordered flex items-center gap-2')}>
+        <input 
+          className={cn('grow' )}
+          {...register('password', { onChange: handleInputChange })}
+            placeholder=	{( isSubmitting ) 
+              ? "Processing" 
+              : 'password'}
+          />
+          {errors.password && <div>{errors.password.message}</div>}
+      </label>
+      <button
+        className='AuthFormSubmitBtn mt-auto btn btn-active btn-outline w-full'
+        type='submit'
+        disabled={isSubmitting || !isDirty || !isValid || !!logError}
+            >
+        { isLoading  ? "Sending.." :  "Sign Up.." }
+      </button>
+
+    </form>
   )
 }
 

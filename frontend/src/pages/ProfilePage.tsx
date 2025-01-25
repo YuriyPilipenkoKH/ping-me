36916de5c0a875b55f9cc42ff88be2f5aa8077ea
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-25 13:17:00 +0200

    handleImageUpload func

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -5,6 +5,22 @@ import { useAuthStore } from '../store/useAuthStore';
 const ProfilePage = () => {
   const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
   const [selectedImg, setSelectedImg] = useState(null);
+  
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = async () => {
+      const base64Image = reader.result;
+      setSelectedImg(base64Image);
+      await updateProfile({ profilePic: base64Image });
+    };
+  };
+
   return (
     <div>ProfilePage</div>
   )

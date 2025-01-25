17b6a0981b0bcfc5a1bd64a9ce8648033c80fda6
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-25 13:15:46 +0200

    setting state in profile page

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Camera, Mail, User } from "lucide-react";
+import { useAuthStore } from '../store/useAuthStore';
 
 const ProfilePage = () => {
+  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div>ProfilePage</div>
   )

d71956143409c26c6cd4a420e0ffab6181d78fe2
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-26 17:59:44 +0200

    sidebar markup

--- frontend/src/components/home/Sidebar.tsx
+++ frontend/src/components/home/Sidebar.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { useChatStore } from '../../store/useChatStore'
+import SidebarSkeleton from '../skeletons/SidebarSkeleton'
 
 const Sidebar = () => {
  const {
@@ -10,7 +11,7 @@ const Sidebar = () => {
     isUsersLoading
     } = useChatStore()
     const onlineUsers =[]
-    
+
     useEffect(() => {
       getUsers();
     }, [getUsers]);

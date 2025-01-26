804ac5095efb0c1c82648a22d6efb40b4065471c
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-26 06:49:23 +0200

    separeted AvatarUploadForm from profile page

--- frontend/src/components/forms/AvatarUploadForm.orig.tsx
+++ frontend/src/components/forms/AvatarUploadForm.orig.tsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthStore } from '../../store/useAuthStore';
+import { Camera, } from "lucide-react";
+import toast from 'react-hot-toast';
 
 const AvatarUploadForm = () => {
+  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+    const [selectedImg, setSelectedImg] =  useState<string | null>(null);
+
+    const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files?.[0]; // Use optional chaining to handle null or undefined
+      if (!file) return;
+        // Check if the file size is too large (e.g., 5MB max)
+      if (file.size > 5 * 1024 * 1024) { // 5MB
+        toast.error("File size exceeds the limit of 5MB.");
+        return;
+      }
+  
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+  
+      reader.onload = async () => {
+        const base64Image = reader.result as string; // Type assertion because result can be string or ArrayBuffer
+        setSelectedImg(base64Image);
+        await updateProfile({ image: base64Image });
+      };
+    };
   return (
-    <div>AvatarUploadForm</div>
+    <div className="flex flex-col items-center gap-4">
+    <div className="relative">
+      <img
+        src={selectedImg || authUser?.image || "/avatar.png"}
+        alt="Profile"
+        className="size-32 rounded-full object-cover border-4 "
+      />
+      <label
+        htmlFor="avatar-upload"
+        className={`
+          absolute bottom-0 right-0 
+          bg-base-content hover:scale-105
+          p-2 rounded-full cursor-pointer 
+          transition-all duration-200
+          ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
+        `}
+      >
+        <Camera className="w-5 h-5 text-base-200" />
+        <input
+          type="file"
+          id="avatar-upload"
+          className="hidden"
+          accept="image/*"
+          onChange={handleImageUpload}
+          disabled={isUpdatingProfile}
+        />
+      </label>
+    </div>
+    <p className="text-sm text-zinc-400">
+      {isUpdatingProfile ? "Uploading..." : "Click the camera icon to update your photo"}
+    </p>
+  </div>
+
   )
 }
 
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,30 +1,11 @@
 import React, { useState } from 'react'
-import { Camera, Mail, User } from "lucide-react";
+import {  Mail, User } from "lucide-react";
 import { useAuthStore } from '../store/useAuthStore';
-import toast from 'react-hot-toast';
+import AvatarUploadForm from '../components/forms/AvatarUploadForm.orig';
 
 const ProfilePage = () => {
-  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
-  const [selectedImg, setSelectedImg] =  useState<string | null>(null);
+  const { authUser, } = useAuthStore();
 
-  const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0]; // Use optional chaining to handle null or undefined
-    if (!file) return;
-      // Check if the file size is too large (e.g., 5MB max)
-    if (file.size > 5 * 1024 * 1024) { // 5MB
-      toast.error("File size exceeds the limit of 5MB.");
-      return;
-    }
-
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    reader.onload = async () => {
-      const base64Image = reader.result as string; // Type assertion because result can be string or ArrayBuffer
-      setSelectedImg(base64Image);
-      await updateProfile({ image: base64Image });
-    };
-  };
 
   return (
      <div className="h-screen pt-20">
@@ -36,39 +17,7 @@ const ProfilePage = () => {
           </div>
 
           {/* avatar upload section */}
-
-          <div className="flex flex-col items-center gap-4">
-            <div className="relative">
-              <img
-                src={selectedImg || authUser?.image || "/avatar.png"}
-                alt="Profile"
-                className="size-32 rounded-full object-cover border-4 "
-              />
-              <label
-                htmlFor="avatar-upload"
-                className={`
-                  absolute bottom-0 right-0 
-                  bg-base-content hover:scale-105
-                  p-2 rounded-full cursor-pointer 
-                  transition-all duration-200
-                  ${isUpdatingProfile ? "animate-pulse pointer-events-none" : ""}
-                `}
-              >
-                <Camera className="w-5 h-5 text-base-200" />
-                <input
-                  type="file"
-                  id="avatar-upload"
-                  className="hidden"
-                  accept="image/*"
-                  onChange={handleImageUpload}
-                  disabled={isUpdatingProfile}
-                />
-              </label>
-            </div>
-            <p className="text-sm text-zinc-400">
-              {isUpdatingProfile ? "Uploading..." : "Click the camera icon to update your photo"}
-            </p>
-          </div>
+          <AvatarUploadForm />
 
           <div className="space-y-6">
             <div className="space-y-1.5">

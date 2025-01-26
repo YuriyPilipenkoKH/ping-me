6860bc1e2bff22eff05edf71044c528753ff9d39
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-26 07:08:10 +0200

    check for file size

--- frontend/src/components/forms/AvatarUploadForm.custom.tsx
+++ frontend/src/components/forms/AvatarUploadForm.custom.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAuthStore } from "../../store/useAuthStore";
+import toast from "react-hot-toast";
+
 
 const AvatarUploadForm = () => {
+    const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+    const [selectedImg, setSelectedImg] =  useState<string | null>(null);
+    console.log(authUser);
+
+    const handleImageUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files?.[0]; // Use optional chaining to handle null or undefined
+      if (!file) return;
+      if (file.size > 5 * 1024 * 1024) { // 5MB
+        toast.error("File size exceeds the limit of 5MB.");
+        return;
+      }
+      
+    }
+
   return (
-    <div>AvatarUploadForm</div>
+    <form>AvatarUploadForm</form>
   )
 }
 

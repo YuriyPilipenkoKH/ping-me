45423090e80d915cd1a1831291e20cd07dad171e
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-26 07:04:25 +0200

    Refactor ProfileForm to use a form element and add AvatarUploadForm component

---
+++ frontend/src/components/forms/AvatarUploadForm.custom.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AvatarUploadForm = () => {
+  return (
+    <div>AvatarUploadForm</div>
+  )
+}
+
+export default AvatarUploadForm
--- frontend/src/components/forms/ProfileForm.custom.tsx
+++ frontend/src/components/forms/ProfileForm.custom.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+
 
 const ProfileForm = () => {
   return (
-    <div>ProfileForm</div>
+    <form>ProfileForm</form>
   )
 }
 

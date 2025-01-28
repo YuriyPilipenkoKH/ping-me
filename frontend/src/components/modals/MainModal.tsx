75b78e43cd8f336f8bb2f8bbc19cd38dbdfb61b2
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-28 16:28:57 +0200

    configured del modal

--- frontend/src/components/modals/MainModal.tsx
+++ frontend/src/components/modals/MainModal.tsx
@@ -1,11 +1,17 @@
 
 import React from 'react'
+import { ModalBaseTypes } from '../../types/modalTypes';
 
-// interface MainModalProps {
-//   id: string
-// }
+interface MainModalProps {
+  modalTypes: ModalBaseTypes
+}
 
-const MainModal = () => {
+const MainModal: React.FC<MainModalProps> = ({modalTypes}) => {
+  const {
+    // modalName, 
+    text, 
+    title,
+} = modalTypes
   const click = () => {
     const modal = document.getElementById('my_modal_3') as HTMLDialogElement | null;
     if (modal) {
@@ -19,13 +25,14 @@ const MainModal = () => {
 <>
   <button className="btn" onClick={click}>open modal</button>
   <dialog id="my_modal_3" className="modal">
+
     <div className="modal-box">
       <form method="dialog">
         {/* if there is a button in form, it will close the modal */}
         <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
       </form>
-      <h3 className="font-bold text-lg">Hello!</h3>
-      <p className="py-4">Press ESC key or click on ✕ button to close</p>
+      <h3 className="font-bold text-lg">{title}</h3>
+      <p className="py-4">{text}</p>
     </div>
   </dialog>
 </>
---
+++ frontend/src/data/modalProps.ts
@@ -0,0 +1,12 @@
+import { ModalBaseTypes } from "../types/modalTypes";
+
+export const DeletingMessageConfirmProps: ModalBaseTypes = {
+  modalName: 'DeletingMessageConfirm',
+  title: 'Are you sure deleting',
+  text: 'There will be no return....',
+};
+export const EditProfileProps: ModalBaseTypes = {
+  modalName: 'EditProfile',
+  title: 'Update profile',
+  text: 'You can edit anything',
+};

c5e67b19964e4d1d08da1176c5c3f997ef71e4f2
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-29 20:47:37 +0200

    axios modifyed

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axiosInstance  from 'axios'
 
 export const axios = axiosInstance.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
   withCredentials:true,
   headers: { "Content-Type": "application/json" }
 })

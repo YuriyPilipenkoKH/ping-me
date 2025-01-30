1c96d74d726c35fd62009ebba39bc9746ece80f7
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-30 19:12:33 +0200

    rm rf

--- backend/src/lib/multer.js
+++
@@ -1,7 +0,0 @@
-import multer from 'multer';
-
-// Set up multer memory storage configuration
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage }); // multer middleware
-
-export {storage , upload} 

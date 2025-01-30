3ee8fac161a8ca88b072e970d6092fa08ec7f742
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-30 22:34:40 +0200

    refactor: comment out unused build and resolve configurations in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from "path";
+// import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "../public",
-    emptyOutDir: true  
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src")
-    }
-  }
+  // build: {
+  //   outDir: "../public",
+  //   emptyOutDir: true  
+  // },
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "src")
+  //   }
+  // }
 })
 //
 //

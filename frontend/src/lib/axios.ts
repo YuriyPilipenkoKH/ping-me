a53cb37c0f8a11b7ed92184e2fbb6af6b4255e18
Yuriy Pilipenko <yurik2061@gmail.com>
2025-01-30 12:40:51 +0200

    add dotenv for environment variable management in axios configuration

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,14 @@
 import axiosInstance  from 'axios'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const API = process.env.API
+console.log('API',API); 
 
 export const axios = axiosInstance.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : `${API}/api`,
+  baseURL:  `${API}/api`,
   withCredentials:true,
   headers: { "Content-Type": "application/json" }
 })
